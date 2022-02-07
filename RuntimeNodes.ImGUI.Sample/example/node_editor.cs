3a79ee364cf64adb1c28cd99df4e6f72d45f465b
Yannick Triqueneaux <yannick.triqueneaux@ubisoft.com>
2022-02-06 22:01:32 -0500

    Added ImNodes C++ example files content to Runtime.Nodes.ImGui.Sample  (in order to learn ImNodes)

---
+++ RuntimeNodes.ImGUI.Sample/example/node_editor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeNodes.ImGUI.Sample.example
+{
+    class node_editor
+    {
+        #pragma once
+
+namespace example
+{
+void NodeEditorInitialize();
+void NodeEditorShow();
+void NodeEditorShutdown();
+} // namespace example
+    }
+}
